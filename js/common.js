f5e4b890f91f96900cbbf8ef5bc9dcf110de0dfe
sweet-bei <1847959118@qq.com>
2024-01-10 22:37:27 +0800

    初始化博客界面

---
+++ js/common.js
@@ -0,0 +1,6 @@
+var bgc_style = document.getElementById("bgc_style");
+bgc_style.onclick = function () {
+    document.body.style.backgroundColor = "#000"
+    document.body.style.color = "#fff"
+    this.className = "iconfont size icon-yueliang"
+};
